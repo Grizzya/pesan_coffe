7186742ac217f77a15f430932410a127e196cd1d
Grizzya <suryaartha971@gmail.com>
2025-12-10 23:05:57 +0800

    sebelum mitrans

--- app/api/Payment/create/route.ts
+++ app/api/Payment/create/route.ts
@@ -1,37 +1,61 @@
+// export const runtime = "nodejs";
+
+// import { NextResponse } from "next/server";
+// import midtransClient from "midtrans-client";
+
+
+
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
+
+//     const snap = new midtransClient.Snap({
+//       isProduction: false,
+//       serverKey: process.env.MIDTRANS_SERVER_KEY!,
+//       clientKey: process.env.MIDTRANS_CLIENT_KEY!,
+//     });
+
+//     const parameter = {
+//       transaction_details: {
+//         order_id: `ORDER-${Date.now()}`,
+//         gross_amount: body.total,
+//       },
+//       customer_details: {
+//         first_name: body.name,
+//         email: body.email,
+//       },
+//       item_details: body.items,           
+//     };
+
+//     const transaction = await snap.createTransaction(parameter);
+
+//     return NextResponse.json({
+//       token: transaction.token,
+//       redirect_url: transaction.redirect_url,
+//     });
+
+//   } catch (error) {
+//     console.error("Error Midtrans:", error);
+//     return NextResponse.json({ error: "Midtrans error" }, { status: 500 });
+//   }
+// }
+
+
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
-import midtransClient from "midtrans-client";
 
 export async function POST(req: Request) {
   try {
-    const body = await req.json();
-
-    const snap = new midtransClient.Snap({
-      isProduction: false,
-      serverKey: process.env.MIDTRANS_SERVER_KEY!,
-      clientKey: process.env.MIDTRANS_CLIENT_KEY!,
-    });
-
-    const parameter = {
-      transaction_details: {
-        order_id: `ORDER-${Date.now()}`,
-        gross_amount: body.total,
-      },
-      customer_details: {
-        first_name: body.name,
-        email: body.email,
-      },
-      item_details: body.items,           
-    };
-
-    const transaction = await snap.createTransaction(parameter);
-
+    const body = await req.json(); // menerima JSON
+    console.log("API /api/payment/create called — body:", JSON.stringify(body));
     return NextResponse.json({
-      token: transaction.token,
-      redirect_url: transaction.redirect_url,
+      ok: true,
+      message: "route OK",
+      received: body
     });
-
-  } catch (error) {
-    console.error("Error Midtrans:", error);
-    return NextResponse.json({ error: "Midtrans error" }, { status: 500 });
+  } catch (err) {
+    console.error("route error:", err);
+    return NextResponse.json({ error: "bad request" }, { status: 400 });
   }
 }
